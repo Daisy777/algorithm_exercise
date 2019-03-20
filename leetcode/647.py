df234763d9e1fe4135e99e2aec4005ce8dac41e6
ZINAN ZHAO <zinanzhao2-c@my.cityu.edu.hk>
2019-03-20 21:21:35 +0800

    add leetcode 647

--- leetcode/647.py
+++ leetcode/647.py
@@ -1,10 +1,22 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        def _countSubstring(s:str, begin:int, end:int) -> int:
-            if begin == 0 or end == len(s)-1:
-                return 0
+        if not s:
+            return 0
             
-            numchar = len(str)
-            dp = [[1 for i in range(numchar)] for j in range(numchar)]
-            for i in range(1, numchar):
-                for j in range(numchar):
+        numchar = len(s)
+        dp = [[1 for i in range(numchar)] for j in range(numchar)]
+        for i in range(1, numchar):
+            for j in range(numchar):
+                if i+j > numchar-1:
+                    break
+                
+                if s[j] == s[i+j]:
+                    dp[j][i+j] = dp[j+1][i+j-1]
+                else:
+                    dp[j][i+j] = 0
+        return sum([x for row in dp for x in row])-int(numchar*numchar/2-numchar/2)
+
+# test
+if __name__ == '__main__':
+    print(Solution().countSubstrings("aaa"))
+    print(Solution().countSubstrings('abc'))
